3803a0cf34ecdb81b6613759416a7cd009fd5f9c
Yury Ushakov <yury.ushakov@icloud.com>
2026-03-26 13:31:00 +0000

    Add users to the admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, EmailVerification
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ['email']
+    list_display = ['email', 'is_staff', 'is_active', 'date_joined']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('email', 'password1', 'password2')}),
+    )
+    search_fields = ['email']
+    filter_horizontal = ('groups', 'user_permissions')
+
+
+@admin.register(EmailVerification)
+class EmailVerificationAdmin(admin.ModelAdmin):
+    list_display = ['email', 'code', 'is_used', 'created_at']
+    readonly_fields = ['created_at']
