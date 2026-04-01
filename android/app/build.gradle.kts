f040a8f54435136b7a7ef4dfd5a12ea7e0cc42e0
Yury Ushakov <yury.ushakov@icloud.com>
2026-04-01 21:25:28 +0100

    Update verions code and name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "gallery.eliza.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
